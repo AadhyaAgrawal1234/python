6201bb0b4178858482913fd147cf36a5ba55ddb2
AadhyaAgrawal1234 <sweetncuteaadhya@gmail.com>
2025-10-31 17:04:56 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,9 @@
+radius = float(input("Enter the radius of the circle: "))
+
+pi = 3.14
+
+area = pi * radius * radius
+perimeter = 2 * pi * radius
+
+print("Area of the circle =", area)
+print("Perimeter of the circle =", perimeter)
