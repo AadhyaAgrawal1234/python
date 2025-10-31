a84322514171bbb6433109f24089e4bcfcfae7aa
AadhyaAgrawal1234 <sweetncuteaadhya@gmail.com>
2025-10-31 17:37:00 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+product = num1 * num2
+
+print("The sum of the two numbers is:", sum)
+print("The product of the two numbers is:", product)
